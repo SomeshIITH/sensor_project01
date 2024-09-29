e64677d3eb48b2a2abd1bb7ed42027330337c10d
SomeshIITH <shivjaiswal51230@gmail.com>
2024-09-29 21:23:17 +0530

    complted all modules

---
+++ upload_data.py
@@ -0,0 +1,23 @@
+
+
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+#url
+uri="mongodb+srv://comrades01012001:sEi8M6VNZJnLCcIE@cluster0.8siet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+#create a new client and connectt to server
+client = MongoClient(uri)
+
+#create database name and collection name
+DATABASE_NAME="pwskills"
+COLLECTION_NAME='waferfault'
+
+df=pd.read_csv(r"C:\Users\manas\Desktop\python pw\sensor project\notebook\wafer_23012020_041211.csv")
+
+df=df.drop("Unnamed: 0",axis=1)
+
+json_record=list(json.loads(df.T.to_json()).values())
+
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
